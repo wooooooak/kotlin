c758f4a2d080b767af12e634ef0f8ca04b1806be
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-08-05 15:27:59 +0200

    ~ move statements to anonymous initializers

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/ir/builders/declarations/declarationBuilders.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/ir/builders/declarations/declarationBuilders.kt
@@ -157,6 +157,17 @@ internal fun IrFactory.buildConstructor(
     }
 }
 
+@PublishedApi
+internal fun IrFunctionBuilder.buildAnonymousInitializer(originalDescriptor: ClassDescriptor?): IrAnonymousInitializer {
+    val wrappedDescriptor =
+        if (originalDescriptor != null) WrappedClassDescriptor(originalDescriptor.annotations, originalDescriptor.source)
+        else WrappedClassDescriptor()
+    return IrAnonymousInitializerImpl(
+        startOffset, endOffset, origin,
+        IrAnonymousInitializerSymbolImpl(wrappedDescriptor)
+    )
+}
+
 inline fun IrFactory.buildFun(
     originalDescriptor: FunctionDescriptor? = null, builder: IrFunctionBuilder.() -> Unit
 ): IrSimpleFunction =
@@ -214,6 +225,24 @@ inline fun IrClass.addConstructor(builder: IrFunctionBuilder.() -> Unit = {}): I
     }.also { constructor ->
         declarations.add(constructor)
         constructor.parent = this@addConstructor
+    }
+
+inline fun buildAnonymousInitializer(
+    originalDescriptor: ClassDescriptor? = null,
+    builder: IrFunctionBuilder.() -> Unit
+): IrAnonymousInitializer =
+    IrFunctionBuilder().run {
+        builder()
+        buildAnonymousInitializer(originalDescriptor)
+    }
+
+inline fun IrClass.addAnonymousInitializer(builder: IrFunctionBuilder.() -> Unit = {}): IrAnonymousInitializer =
+    buildAnonymousInitializer {
+        builder()
+        returnType = defaultType
+    }.also { anonymousInitializer ->
+        declarations.add(anonymousInitializer)
+        anonymousInitializer.parent = this@addAnonymousInitializer
     }
 
 private val RECEIVER_PARAMETER_NAME = Name.special("<this>")
