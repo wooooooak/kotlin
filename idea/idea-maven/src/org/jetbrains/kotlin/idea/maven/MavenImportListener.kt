e53f70e452c9b323cb195eacb65d043ab2e9fa6e
Yunir Salimzyanov <yunir.salimzyanov@jetbrains.com>
2020-08-17 10:51:33 +0300

    -- 192 TBD maven

--- idea/idea-maven/src/org/jetbrains/kotlin/idea/maven/MavenImportListener.kt
+++ idea/idea-maven/src/org/jetbrains/kotlin/idea/maven/MavenImportListener.kt
@@ -15,7 +15,6 @@ import org.jetbrains.kotlin.idea.configuration.KotlinMigrationProjectService
 import org.jetbrains.kotlin.idea.configuration.notifyOutdatedBundledCompilerIfNecessary
 import org.jetbrains.kotlin.idea.util.ProgressIndicatorUtils.runUnderDisposeAwareIndicator
 
-// FIX ME WHEN BUNCH 192 REMOVED
 class MavenImportListener : StartupActivity {
 
     override fun runActivity(project: Project) {
@@ -37,6 +36,4 @@ class MavenImportListener : StartupActivity {
             }
         })
     }
-
-
 }
